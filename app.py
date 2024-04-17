6eca660fa0dce82cadf2110e46d5aacd98877d2b
Prakhar Pandey <personal.prakhar@gmail.com>
2024-04-17 14:47:02 +0530

    Updated Jobs data loading function

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 from database import load_jobs 
 app = Flask(__name__)
-JOBS = load_jobs()
-print(JOBS)
 @app.route('/')
 def helloWord():
+    JOBS = load_jobs()
     return render_template('home.html', jobs=JOBS, company='Jovian Careers')
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
