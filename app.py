2a5bdd6dd625e1f29c256aaddf1636e1e0c75a8c
Prakhar Pandey <personal.prakhar@gmail.com>
2024-04-29 23:59:05 +0530

    **

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import RequestEntityTooLarge
 from static.uploads.path import getDirectory
 import os
 app = Flask(__name__)
-UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'uploads') #the best way to give path. Works on development as well as production server
+UPLOAD_FOLDER = '/opt/render/project/src/static/uploads' #the best way to give path. Works on development as well as production server
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024  # 1MB
@@ -16,7 +16,8 @@ def allowed_file(filename):
 
 @app.route('/')
 def helloWord():
-    print(getDirectory())
+    current_file_path = os.path.abspath(__file__)
+    print(current_file_path)
     ALLJOBS = load_jobs()
     return render_template('home.html', jobs=ALLJOBS)
 
